800c8c184a3609f6a3ac88305aae4f8a8fb37a43
An Nguyen Thanh <an.nguyenthanh@axonactive.com>
2018-11-28 18:27:08 +0700

    Home screen, scan color screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Content, Text, Header, Body, Title, Right, Button, Icon } from "native-base";
+import { StyleSheet } from 'react-native';
+import { Container, Content, Header, Body, Title, Right, Button, Icon, Grid, Col, Row } from "native-base";
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
@@ -21,9 +22,49 @@ export default class HomeScreen extends React.Component {
     return (
       <Container>
         <Content padder>
-          <Text>Home Screen</Text>
+          <Grid>
+            <Row>
+              <Col style={styles.col}>
+                <Button bordered dark full style={styles.button}
+                  onPress={() => this.props.navigation.navigate("ScanColor")}
+                >
+                  <Icon type="FontAwesome" name="camera" style={styles.icon} />
+                </Button>
+              </Col>
+              <Col style={styles.col}>
+                <Button bordered dark full style={styles.button}>
+                  <Icon type="FontAwesome" name="shopping-cart" style={styles.icon} />
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col style={styles.col}>
+                <Button bordered dark full style={styles.button}>
+                  <Icon type="FontAwesome" name="newspaper-o" style={styles.icon} />
+                </Button>
+              </Col>
+              <Col style={styles.col}>
+                <Button bordered dark full style={styles.button}>
+                  <Icon type="FontAwesome" name="comments" style={styles.icon} />
+                </Button>
+              </Col>
+            </Row>
+          </Grid>
         </Content>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  col: {
+    height: 150,
+    padding: 8,
+  },
+  button: {
+    height: 134,
+  },
+  icon: {
+    fontSize: 50,
+  }
+});
--- screens/ScanColorScreen.js
+++ screens/ScanColorScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Container, Content, Text, Header, Body, Title, Right, Button, Icon, Left } from "native-base";
+import { TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
+import { Text, Header, Body, Title, Right, Button, Icon, Left, View } from "native-base";
 
 export default class ScanColorScreen extends React.Component {
   static navigationOptions = ({ navigation }) => ({
     header: (
       <Header>
         <Left>
-          <Button onPress={() => navigation.goBack()}>
+          <Button transparent onPress={() => navigation.goBack()}>
             <Icon name="arrow-back" />
           </Button>
         </Left>
@@ -15,20 +17,61 @@ export default class ScanColorScreen extends React.Component {
         </Body>
         <Right>
           <Button transparent onPress={() => navigation.navigate("Settings")}>
-            <Icon name="setting" />
+            <Icon name="settings" />
           </Button>
         </Right>
       </Header>
     ),
   });
 
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
   render() {
-    return (
-      <Container>
-        <Content padder>
-          <Text>Scan Color Screen</Text>
-        </Content>
-      </Container>
-    );
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
   }
 }
